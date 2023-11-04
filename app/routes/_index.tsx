f7a7f43c7499f416bbc63f5cfca91ff395b1d34c
Jonathan Clark <contact@refactorthis.dev>
2023-11-03 23:29:54 -0700

    fix: change index style

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,21 @@
-import {LegacyCard, EmptyState} from '@shopify/polaris';
+import { Page, Card, EmptyState, Layout } from '@shopify/polaris';
 
 
 export default function Index() {
   return (
-    <LegacyCard sectioned>
-      <EmptyState
-        heading="There's nothing here captain!"
-        image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
-      >
-        <p>
-          Congratulations on following the Readme and getting this far! Now it's time to build something awesome.
-        </p>
-      </EmptyState>
-    </LegacyCard>
+    <Page>
+      <Layout>
+        <Card>
+          <EmptyState
+            heading="There's nothing here captain!"
+            image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+          >
+            <p>
+              Congratulations on following the Readme and getting this far! Now it's time to build something awesome.
+            </p>
+          </EmptyState>
+        </Card>
+      </Layout>
+    </Page>
   );
 }
