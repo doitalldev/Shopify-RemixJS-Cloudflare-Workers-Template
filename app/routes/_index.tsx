8677986259fe74138ab6f713da9b5a43c50874ad
Jonathan Clark <contact@refactorthis.dev>
2023-11-03 17:19:28 -0700

    feat: better index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,17 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import {LegacyCard, EmptyState} from '@shopify/polaris';
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
 
 export default function Index() {
   return (
-    <div>
-      Welcome!
-    </div>
+    <LegacyCard sectioned>
+      <EmptyState
+        heading="There's nothing here captain!"
+        image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+      >
+        <p>
+          Congratulations on following the Readme and getting this far! Now it's time to build something awesome.
+        </p>
+      </EmptyState>
+    </LegacyCard>
   );
 }
