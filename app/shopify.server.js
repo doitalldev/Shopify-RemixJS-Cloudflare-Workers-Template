845e2522f2d59eeccd953a1bd0b2b9a92949e564
Jonathan Clark <contact@refactorthis.dev>
2024-04-13 23:07:05 -0700

    clean: format and remove excess code

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -1,38 +1,36 @@
-import { restResources } from "@shopify/shopify-api/rest/admin/2023-10";
 import {
-  AppDistribution,
   DeliveryMethod,
   shopifyApp,
   LATEST_API_VERSION,
 } from '@shopify/shopify-app-remix';
 
-import {DrizzleSessionStorageSQLite} from '@shopify/shopify-app-session-storage-drizzle';
+import { DrizzleSessionStorageSQLite } from '@shopify/shopify-app-session-storage-drizzle';
 import { drizzle } from "drizzle-orm/d1";
 
-import {sessionTable} from 'schema';
+import { sessionTable } from 'schema';
 
 export * as schema from "schema";
 
-export const shopify = (env) =>  {
-const d1Session = new DrizzleSessionStorageSQLite(drizzle(env.DB), sessionTable);
+export const shopify = (env) => {
+  const d1Session = new DrizzleSessionStorageSQLite(drizzle(env.DB), sessionTable);
 
-return shopifyApp({
-  apiKey: env.SHOPIFY_APP_KEY,
-  apiSecretKey: env.SHOPIFY_APP_SECRET,
-  appUrl: env.APP_URL,
-  scopes: env.SHOPIFY_APP_SCOPES.split(","),
-  apiVersion: LATEST_API_VERSION,
-  isEmbeddedApp: true,
-  future: {
-    unstable_newEmbeddedAuthStrategy: true,
-  },
-  sessionStorage: d1Session,
-  authPathPrefix: "/auth",
-  webhooks: {
-    APP_UNINSTALLED: {
-      deliveryMethod: DeliveryMethod.Http,
-      callbackUrl: "/webhooks",
+  return shopifyApp({
+    apiKey: env.SHOPIFY_APP_KEY,
+    apiSecretKey: env.SHOPIFY_APP_SECRET,
+    appUrl: env.APP_URL,
+    scopes: env.SHOPIFY_APP_SCOPES.split(","),
+    apiVersion: LATEST_API_VERSION,
+    isEmbeddedApp: true,
+    future: {
+      unstable_newEmbeddedAuthStrategy: true,
     },
-  },
-});
+    sessionStorage: d1Session,
+    authPathPrefix: "/auth",
+    webhooks: {
+      APP_UNINSTALLED: {
+        deliveryMethod: DeliveryMethod.Http,
+        callbackUrl: "/webhooks",
+      },
+    },
+  });
 }
